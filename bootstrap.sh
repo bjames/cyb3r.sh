06978c10430e63ba0e2631c2df273e53b0c370f2
Brandon James <brandon@brandonsjames.com>
2025-06-03 22:19:23 -0600

    update css

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+# dnf update + reboot first
+dnf install epel-release -y
+dnf install pandoc git -y
+dnf install nginx -y
+systemctl start nginx
+systemctl enable nginx
+cd /usr/share/nginx
+git clone https://github.com/bjames/cyb3r.sh.git
