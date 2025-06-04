363953bf4769a9ea7508acef8590480b742195b1
Brandon James <brandon@brandonsjames.com>
2025-06-04 17:48:24 -0600

    basic status page

--- status.sh
+++ status.sh
@@ -1,6 +1,6 @@
 echo "Status report generated at $(date)" > /usr/share/nginx/cyb3r.sh/site/status.txt
 echo "----------------" >> /usr/share/nginx/cyb3r.sh/site/status.txt
-uptime -p > /usr/share/nginx/cyb3r.sh/site/status.txt
+uptime -p >> /usr/share/nginx/cyb3r.sh/site/status.txt
 shutdown --show 2>&1 | awk -F ',' '{ print $1 }' >> /usr/share/nginx/cyb3r.sh/site/status.txt
 echo "----------------" >> /usr/share/nginx/cyb3r.sh/site/status.txt
 echo "last commit" >> /usr/share/nginx/cyb3r.sh/site/status.txt
