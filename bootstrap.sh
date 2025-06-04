c6fe53d5b20c90d12e12531db60ae8c551e6211a
Brandon James <brandon@brandonsjames.com>
2025-06-03 23:04:34 -0600

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,4 @@ systemctl start nginx
 systemctl enable nginx
 cd /usr/share/nginx
 git clone https://github.com/bjames/cyb3r.sh.git
+echo "*/1 * * * * git -C /usr/share/nginx/cyb3r.sh pull" | EDITOR="tee -a" crontab -e
