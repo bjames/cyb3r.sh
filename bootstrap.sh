6c6192ff04f6e8db3182f4241c0ae8a49db45450
Brandon James <brandon@brandonsjames.com>
2025-06-04 19:46:35 -0600

    absolute path to shutdown

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ systemctl enable nginx
 cd /usr/share/nginx
 git clone https://github.com/bjames/cyb3r.sh.git
 echo "*/1 * * * * git -C /usr/share/nginx/cyb3r.sh pull" | EDITOR="tee -a" crontab -e
+echo "*/5 * * * * /usr/share/nginx/cyb3r.sh/status.sh" | EDITOR="tee -a" crontab -e
 # nginx config changes
 dnf install dnf-automatic -y
 # dnf automatic config changes (apply-updates = yes, reboot = when-needed, reboot-command = )
